ecbb5b6ef2e35875ba868bff02673a623732a529
BNaz-OTU <bilal.nazar@ontariotechu.net>
2025-02-05 21:23:08 -0500

    Readjusted format of projects

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -2,78 +2,27 @@
 import React from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
-import proj1 from "../assets/projects/NewProj1.png";
-import proj2 from "../assets/projects/NewProj2.png";
-import proj3 from "../assets/projects/NewProj3.png";
-import proj4 from "../assets/projects/NewProj4.png";
-import proj5 from "../assets/projects/NewProj5.png";
-import proj6 from "../assets/projects/NewProj6_1.png";
-import proj7 from "../assets/projects/NewProj7.png";
-import proj8 from "../assets/projects/NewProj8.png";
-import proj9 from "../assets/projects/NewProj9.png";
-import proj10 from "../assets/projects/onboarding3.png";
-import proj11 from "../assets/projects/cards.png";
+import VideoProj from "../assets/projects/NewProj1.png";
+import LiveProj from "../assets/projects/NewProj2.png";
+import HotelProj from "../assets/projects/NewProj3.png";
+import AsciiProj from "../assets/projects/NewProj4.png";
+import NASAproj from "../assets/projects/NewProj5.png";
+import BasketProj from "../assets/projects/NewProj6_1.png";
+import FizziProj from "../assets/projects/NewProj7.png";
+import RetailProj from "../assets/projects/NewProj8.png";
+import TennisProj from "../assets/projects/NewProj9.png";
+import UberProj from "../assets/projects/onboarding3.png";
+import AoraProj from "../assets/projects/cards.png";
 
 const projects = [
   {
-    title: "YOOM",
-    desc: "Constructed a video communication platform that adheres to responsive design principles and includes features like meetings, scheduling, recording, screen sharing, and participant management using Next.JS, TypeScript, and GetStream, ensuring an optimal user experience.",
-    devStack: "Next.JS | TypeScript | Tailwind | CSS | HTML | JavaScript",
-    link: "https://bn-zoom-clone.vercel.app/",
-    git: "https://github.com/BNaz-OTU/BN-ZOOM-clone",
-    src: proj1,
-  },
-  {
-    title: "LiveDocs",
-    desc: "Engineered a real-time collaborative online word processor using Next.JS, TypeScript, and LiveBlocks, enabling multi-user editing, presence tracking, and a notification system for document sharing, comments, and activity updates.",
+    title: "Computer Vision Tennis Analysis",
+    desc: "Used Ultralytics for object detection in images and videos to track dynamic elements like tennis players and the ball. Developed and trained a CNN with PyTorch to extract key points from court imagery, creating a detailed court map with OpenCV and analyzing player and ball velocities.",
     devStack:
-      "Next.JS | TypeScript | JavaScript | Tailwind CSS | HTML | LiveBlocks | Lexical Editor | ShadCN",
-    link: "https://bn-live-docs.vercel.app/",
-    git: "https://github.com/BNaz-OTU/BN_Live_Docs",
-    src: proj2,
-  },
-  {
-    title: "Hotel Management Project",
-    desc: "Collaborated on a hotel management system using Java, Spring Boot, Spring Data JPA, and H2. Implemented room management, bookings, and customer services, applying Clean Architecture and MVC for a modular, maintainable codebase. Debugged and resolved issues to support continuous development.",
-    devStack:
-      "Java | Maven | Spring Boot | H2 | Spring Data JPA | ThymleLeaf | HTML | CSS",
-    link: "https://drive.google.com/file/d/1lNEvZy95et4TXu6dPgeJXw4l95Sj5X8T/view",
-    git: "https://github.com/BNaz-OTU/HotelManagementProject",
-    src: proj3,
-  },
-  {
-    title: "Image to ASCII",
-    desc: "Developed a Python application that enables the conversion of images into ASCII art, leveraging image processing techniques to transform selected images into text-based visual representations for enhanced user interaction and display.",
-    devStack: "Python",
-    link: "https://github.com/BNaz-OTU/IMG_to_ASCII",
-    git: "https://github.com/BNaz-OTU/IMG_to_ASCII",
-    src: proj4,
-  },
-  {
-    title: "NASA Astronomy Picture of the Day",
-    desc: "Developed a React application that integrates with the NASA API to dynamically display the Astronomy Picture of the Day. The app fetches and updates daily, presenting the latest astronomical imagery along with a detailed description to enhance user engagement and information delivery.",
-    devStack: "React.JS | HTML | CSS | JavaScript",
-    link: "https://bn-nasa-apod-react-proj.netlify.app/",
-    git: "https://github.com/BNaz-OTU/bn-nasa-react-proj",
-    src: proj5,
-  },
-  {
-    title: "Basketball Data Analysis",
-    desc: "Cleaned and processed data by handling missing values, normalizing variables, and addressing outliers. Used NumPy and Pandas for advanced calculations and analysis, identifying key patterns and trends. Integrated visualizations with Matplotlib and Seaborn, including box and scatter plots, to enhance data interpretation.",
-    devStack:
-      "Python | Jupyter Notebook | Numpy | Scipy | Pandas | Matplotlib | Markdown",
-    link: "https://github.com/BNaz-OTU/basketball-data-analysis",
-    git: "https://github.com/BNaz-OTU/basketball-data-analysis",
-    src: proj6,
-  },
-  {
-    title: "Retail Data K-Clustering Analysis",
-    desc: "Cleaned and processed data by handling missing values, normalizing variables, and addressing outliers. Utilized Scikit-learn and Pandas for advanced analysis, including K-Means clustering, to identify customer shopping patterns. Created visualizations with Matplotlib and Seaborn (box plots, violin plots, scatter plots) to highlight key metrics and improve data comprehension.",
-    devStack:
-      "Python | Scikit-learn | Pandas | Matplotlib | Seaborn | Jupyter Notebook | Markdown",
-    link: "https://github.com/BNaz-OTU/Retail_Data_Clustering",
-    git: "https://github.com/BNaz-OTU/Retail_Data_Clustering",
-    src: proj8,
+      "Python | PyTorch | Jupyter Notebooks | OpenCV | Ultralytics | Pandas | Matplotlib | NumPy",
+    link: "https://github.com/BNaz-OTU/Tennis-Analysis",
+    git: "https://github.com/BNaz-OTU/Tennis-Analysis",
+    src: TennisProj,
   },
   {
     title: "Fizzi E-Commerce Website",
@@ -82,16 +31,24 @@ const projects = [
       "React | Next.JS | TypeScript | Three.JS | GSAP | Prismic | Tailwind CSS",
     link: "https://fizzi-bn.vercel.app/",
     git: "https://github.com/BNaz-OTU/fizzi-bn",
-    src: proj7,
+    src: FizziProj,
   },
   {
-    title: "Computer Vision Tennis Analysis",
-    desc: "Used Ultralytics for object detection in images and videos to track dynamic elements like tennis players and the ball. Developed and trained a CNN with PyTorch to extract key points from court imagery, creating a detailed court map with OpenCV and analyzing player and ball velocities.",
+    title: "Retail Data K-Clustering Analysis",
+    desc: "Cleaned and processed data by handling missing values, normalizing variables, and addressing outliers. Utilized Scikit-learn and Pandas for advanced analysis, including K-Means clustering, to identify customer shopping patterns. Created visualizations with Matplotlib and Seaborn (box plots, violin plots, scatter plots) to highlight key metrics and improve data comprehension.",
     devStack:
-      "Python | PyTorch | Jupyter Notebooks | OpenCV | Ultralytics | Pandas | Matplotlib | NumPy",
-    link: "https://github.com/BNaz-OTU/Tennis-Analysis",
-    git: "https://github.com/BNaz-OTU/Tennis-Analysis",
-    src: proj9,
+      "Python | Scikit-learn | Pandas | Matplotlib | Seaborn | Jupyter Notebook | Markdown",
+    link: "https://github.com/BNaz-OTU/Retail_Data_Clustering",
+    git: "https://github.com/BNaz-OTU/Retail_Data_Clustering",
+    src: RetailProj,
+  },
+  {
+    title: "YOOM",
+    desc: "Constructed a video communication platform that adheres to responsive design principles and includes features like meetings, scheduling, recording, screen sharing, and participant management using Next.JS, TypeScript, and GetStream, ensuring an optimal user experience.",
+    devStack: "Next.JS | TypeScript | Tailwind | CSS | HTML | JavaScript",
+    link: "https://bn-zoom-clone.vercel.app/",
+    git: "https://github.com/BNaz-OTU/BN-ZOOM-clone",
+    src: VideoProj,
   },
   {
     title: "AI Snake Game",
@@ -99,7 +56,7 @@ const projects = [
     devStack: "Python | PyTorch | NumPy | Pygame | Matplotlib",
     link: "https://github.com/BNaz-OTU/ML_snake_game",
     git: "https://github.com/BNaz-OTU/ML_snake_game",
-    src: proj7,
+    src: RetailProj,
   },
   {
     title: "Uber Clone",
@@ -108,7 +65,50 @@ const projects = [
       "TypeScript | React Native | Expo | Stripe | PostgreSQL | Tailwind CSS | Clerk | Google Cloud | zustand | Google Maps",
     link: "https://github.com/BNaz-OTU/bn_uber_clone",
     git: "https://github.com/BNaz-OTU/bn_uber_clone",
-    src: proj10,
+    src: UberProj,
+  },
+  {
+    title: "LiveDocs",
+    desc: "Engineered a real-time collaborative online word processor using Next.JS, TypeScript, and LiveBlocks, enabling multi-user editing, presence tracking, and a notification system for document sharing, comments, and activity updates.",
+    devStack:
+      "Next.JS | TypeScript | JavaScript | Tailwind CSS | HTML | LiveBlocks | Lexical Editor | ShadCN",
+    link: "https://bn-live-docs.vercel.app/",
+    git: "https://github.com/BNaz-OTU/BN_Live_Docs",
+    src: LiveProj,
+  },
+  {
+    title: "Hotel Management Project",
+    desc: "Collaborated on a hotel management system using Java, Spring Boot, Spring Data JPA, and H2. Implemented room management, bookings, and customer services, applying Clean Architecture and MVC for a modular, maintainable codebase. Debugged and resolved issues to support continuous development.",
+    devStack:
+      "Java | Maven | Spring Boot | H2 | Spring Data JPA | ThymleLeaf | HTML | CSS",
+    link: "https://drive.google.com/file/d/1lNEvZy95et4TXu6dPgeJXw4l95Sj5X8T/view",
+    git: "https://github.com/BNaz-OTU/HotelManagementProject",
+    src: HotelProj,
+  },
+  {
+    title: "Basketball Data Analysis",
+    desc: "Cleaned and processed data by handling missing values, normalizing variables, and addressing outliers. Used NumPy and Pandas for advanced calculations and analysis, identifying key patterns and trends. Integrated visualizations with Matplotlib and Seaborn, including box and scatter plots, to enhance data interpretation.",
+    devStack:
+      "Python | Jupyter Notebook | Numpy | Scipy | Pandas | Matplotlib | Markdown",
+    link: "https://github.com/BNaz-OTU/basketball-data-analysis",
+    git: "https://github.com/BNaz-OTU/basketball-data-analysis",
+    src: BasketProj,
+  },
+  {
+    title: "Image to ASCII",
+    desc: "Developed a Python application that enables the conversion of images into ASCII art, leveraging image processing techniques to transform selected images into text-based visual representations for enhanced user interaction and display.",
+    devStack: "Python",
+    link: "https://github.com/BNaz-OTU/IMG_to_ASCII",
+    git: "https://github.com/BNaz-OTU/IMG_to_ASCII",
+    src: AsciiProj,
+  },
+  {
+    title: "NASA Astronomy Picture of the Day",
+    desc: "Developed a React application that integrates with the NASA API to dynamically display the Astronomy Picture of the Day. The app fetches and updates daily, presenting the latest astronomical imagery along with a detailed description to enhance user engagement and information delivery.",
+    devStack: "React.JS | HTML | CSS | JavaScript",
+    link: "https://bn-nasa-apod-react-proj.netlify.app/",
+    git: "https://github.com/BNaz-OTU/bn-nasa-react-proj",
+    src: NASAproj,
   },
   {
     title: "Aora",
@@ -117,7 +117,7 @@ const projects = [
       "React Native | Expo | Nativewind | Tailwind CSS | Animatable | Appwrite",
     link: "https://github.com/BNaz-OTU/BN_ReactNative_proj_Aora",
     git: "https://github.com/BNaz-OTU/BN_ReactNative_proj_Aora",
-    src: proj11,
+    src: AoraProj,
   },
 ];
 
